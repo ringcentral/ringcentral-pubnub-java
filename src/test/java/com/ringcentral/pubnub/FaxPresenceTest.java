ff83c78b4fc07a4b8fad476b5ed7b5c51216c01a
Tyler Long <tyler4long@gmail.com>
2019-10-29 14:49:12 -0700

    Release 1.0.0-beta10

---
+++ src/test/java/com/ringcentral/pubnub/FaxPresenceTest.java
@@ -0,0 +1,41 @@
+package com.ringcentral.pubnub;
+
+import com.ringcentral.RestClient;
+import com.ringcentral.RestException;
+import com.ringcentral.definitions.SubscriptionInfo;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class FaxPresenceTest {
+    @Test
+    public void testPresence() throws IOException, RestException, InterruptedException {
+        RestClient rc = new RestClient(
+                System.getenv("RINGCENTRAL_CLIENT_ID"),
+                System.getenv("RINGCENTRAL_CLIENT_SECRET"),
+                System.getenv("RINGCENTRAL_SERVER_URL")
+        );
+
+        rc.authorize(
+                System.getenv("RINGCENTRAL_USERNAME"),
+                System.getenv("RINGCENTRAL_EXTENSION"),
+                System.getenv("RINGCENTRAL_PASSWORD")
+        );
+        Subscription subscription = new Subscription(rc,
+                new String[]{"/restapi/v1.0/account/~/extension/~/presence?detailedTelephonyState=true&sipData=true"},
+                (jsonString) -> {
+                    System.out.println(jsonString);
+                }
+        );
+        subscription.subscribe();
+        SubscriptionInfo sub = subscription.getSubscription();
+        assert sub != null;
+        assertEquals("Active", sub.status);
+
+        Thread.sleep(512000);
+
+        rc.revoke();
+    }
+}
